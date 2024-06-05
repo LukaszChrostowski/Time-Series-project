b685fb2a116c003b07650a656446b2b4655503e0
LukaszChrostowski <lukchr@st.amu.edu.pl>
2024-06-05 19:34:56 +0100

    add more figures and final model proposal

--- code/project_script.R
+++ code/project_script.R
@@ -13,6 +13,7 @@ library(quantmod)
 library(tidyquant)
 library(dplyr)
 library(forecast)
+library(trend)
 
 # data_microsoft <- read.csv("C:/Users/Miguel Silva/Desktop/universidade/4th year/2nd semester/TS/project/data_microsoft/Microsoft_Stock.csv")
 data_microsoft <- read.csv("~/Desktop/Time-Series-project/data/Microsoft_Stock.csv")
@@ -25,22 +26,40 @@ plot <- chartSeries(data_microsoft)
 # data_microsoft$stock_return <- series_df
 # ts.plot(series)
 
+
 ###############################################
 #  DATA PREPARATION AND ACF ANALYSIS         #
 ###############################################
+
+closed_returns <- data_microsoft$Close
+squared_returns <- closed_returns^2
+
+par(mfrow=c(1,1))
+plot.ts(closed_returns)
+acf(squared_returns, main = "ACF of Squared Closing Price")
+# highly correlated observations
+# data have a trend
+
+# let us consider daily stock returns with log transformation
 log_returns <- tq_transmute(data_microsoft,
                             select = Close,
                             mutate_fun = periodReturn,
                             period = "daily",
                             type = "log")
+squared_log_returns <- log_returns$daily.returns^2
 
-par(mfrow = c(2, 1))
-ts.plot(log_returns) # if trend is visible (it is!), model should be like sarima + garch
+par(mfrow = c(1, 1))
 ts.plot(log_returns$daily.returns, main = "Stock Daily Returns over Time",
         ylab = "Daily Return")
 
-acf(log_returns)
-pacf(log_returns)
+par(mfrow = c(2, 1))
+acf(log_returns$daily.returns, main = "ACF of Daily Returns")
+pacf(log_returns$daily.returns, main = "PACF of Daily Returns")
+
+par(mfrow = c(2, 1))
+acf(squared_log_returns, main = "ACF of Squared Daily Returns")
+pacf(squared_log_returns, main = "PACF of Squared Daily Returns")
+# Conclusions: Observations are highly correlated
 
 # d_series = diff(series)
 # ts.plot(d_series)
@@ -82,35 +101,13 @@ p2 + geom_histogram(aes(x=daily.returns, y=..density..), bins = 100, color="stee
 ########### STATIONARITY ##########
 
 # stationarity - series
-adf.test(log_returns$daily.returns)
-kpss.test(log_returns$daily.returns, null = c("Level", "Trend"), lshort = TRUE)
+# adf.test(log_returns$daily.returns)
+# kpss.test(log_returns$daily.returns, null = c("Level", "Trend"), lshort = TRUE)
 
-# stationarity - first diffs
-# adf.test(d_series) #stationary 
-# kpss.test(d_series, null = c("Level", "Trend"), lshort = TRUE)
-
-###############################################
-#               ACF and PACF                  #
-###############################################
-
-# acf_series = acf(series, lag.max=70, plot=FALSE)
-# plot(acf_series, type="h", xlab="lag", col='red')
-# 
-# pacf_series = pacf(series, lag.max=70, plot=FALSE)
-# plot(pacf_series, type="h", xlab="lag", col='red')
-
-###############################################
-#                DIFFERENTIAL                 #
-###############################################
-
-# ts.plot(d_series)
-# 
-# acf_d_series = acf(d_series, lag.max=70, plot=FALSE)
-# plot(acf_d_series, type="h", xlab="lag", col='red')
-# 
-# pacf_d_series = pacf(d_series, lag.max=70, plot=FALSE)
-# plot(pacf_d_series, type="h", xlab="lag", col='red')
-
+# Trend
+ts.plot(log_returns)
+model <- lm(data = log_returns, daily.returns ~ Date)
+# slowly increasing trend is visible but not significant from statistical point of view
 
 ###############################################
 #                  MODELING                   #
@@ -128,38 +125,161 @@ p3 +
        x="Day",
        y="Volatility")
 
-############# SARIMA #################
-# STEP 1 - Find the most suitable sarima model
-# sarima(log_returns$daily.returns, 0, 0, 2, 0, 0, 0, 0)
+############# ARIMA #################
 
-# autoarima
+# Since we know that we have (?) trend in data - we have to remove it using autoarima
 arima_fit <- auto.arima(log_returns$daily.returns)
+# ARIMA(2,0,1) with non-zero mean 
 residuals_arima <- residuals(arima_fit)
 
+
 ############# GARCH #################
-# STEP 2 - Find the most suitable garch model in respect to choosen sarima model (basing on residuals from SARIMA model)
-# Specify the GARCH model - here we use a basic GARCH(1,1) model
-garch_spec <- ugarchspec(variance.model=list(model="sGARCH",
-                                             garchOrder=c(1,1)),
-                         mean.model=list(armaOrder=c(0,0)),
-                         distribution.model = "norm")
-# just for now - probably needed to find more efficient one
+# function to fit the model
+do_garch <- function(garch.model, garch.r, garch.s, data, dist.model, ar = 0, ma = 0) {
+  # Specify the GARCH model - here we use a basic GARCH(1,1) model
+  garch_spec <- ugarchspec(variance.model=list(model=garch.model,
+                                               garchOrder=c(garch.r, garch.s)),
+                           mean.model=list(armaOrder=c(ar, ma)),
+                           distribution.model = dist.model)
+  # just for now - probably needed to find more efficient one
+  
+  # Fit the GARCH model to the differenced volume series
+  # garch_fit <- ugarchfit(spec = garch_spec, data = log_returns$daily.returns)
+  garch_fit <- ugarchfit(spec = garch_spec,
+                         data = data)
+  
+  ############## RESIDUAL ANALYSIS #################
+  residuals <- residuals(garch_fit, standardize = TRUE)
+  squared_residuals <- residuals^2
+  
+  list(garch_spec = garch_spec,
+       fit = garch_fit,
+       residuals = residuals,
+       squared_residuals = squared_residuals)
+}
+############# Assuming TREND in DATA -> basing on residuals from ARIMA #################
+# (1, 1)
+garch <- do_garch(garch.model = "sGARCH",
+                  garch.r = 1,
+                  garch.s = 1,
+                  data = residuals_arima,
+                  dist.model = "norm")
+print(garch$fit)
 
-# Fit the GARCH model to the differenced volume series
-# garch_fit <- ugarchfit(spec = garch_spec, data = log_returns$daily.returns)
-garch_fit <- ugarchfit(spec = garch_spec, data = residuals_arima)
+# Conclusion from fit
+# 1 No serial correlation between garch residuals
+# Akaike       -5.5615
+# Bayes        -5.5474
 
-# Print the summary of the model fit
-print(garch_fit)
-
-############## RESIDUAL ANALYSIS #################
-residuals <- residuals(garch_fit)
-plot.ts(residuals)
-squared_residuals <- residuals^2
-plot.ts(squared_residuals, main = "Squared Residuals of GARCH model", ylab = "Squared Residuals")
-
-# normality test for residuals
-shapiro.test(as.numeric(residuals))
+par(mfrow = c(1, 1))
+plot.ts(garch$residuals, main = "Residuals of GARCH model", ylab = "Residuals")
+plot.ts(garch$squared_residuals, main = "Squared Residuals of GARCH model", ylab = "Squared Residuals")
 
 # ACF analysis for residuals
-acf(squared_residuals, main = "ACF of squared residuals")
+# This will allow you to check the adequacy of the GARCH(1,1) model by examining whether the residuals
+# and their squared values exhibit any significant autocorrelation. If the ACF and PACF plots show no
+# significant autocorrelation, and have a N(0,1) white noise behaviour, the model is considered a good fit for the data.
+par(mfrow = c(2, 2))
+acf(garch$residuals, main = "ACF of Standardized Residuals")
+pacf(garch$residuals, main = "PACF of Standardized Residuals")
+acf(garch$squared_residuals, main = "ACF of Squared Standardized Residuals")
+pacf(garch$squared_residuals, main = "PACF of Squared Residuals")
+par(mfrow = c(1, 1))
+
+
+############# Assuming NO TREND in DATA #################
+
+# Define the grid for GARCH orders (excluding the (0,0) point)
+garch_orders <- expand.grid(garch.r = 0:2, garch.s = 0:2)
+garch_orders <- garch_orders[!(garch_orders$garch.r == 0 & garch_orders$garch.s == 0), ]
+
+# Define the grid for ARMA orders (0 to 2)
+arma_orders <- expand.grid(ar = 0:2, ma = 0:2)
+
+# Initialize variables to store the results
+results <- data.frame(garch.r = integer(),
+                      garch.s = integer(),
+                      ar = integer(),
+                      ma = integer(),
+                      AIC = numeric(),
+                      BIC = numeric(),
+                      SHIB = numeric(),
+                      HAN = numeric(),
+                      stringsAsFactors = FALSE)
+
+# Nested loops to fit the GARCH models with different GARCH and ARMA orders
+for (i in 1:nrow(garch_orders)) {
+  garch.r <- garch_orders$garch.r[i]
+  garch.s <- garch_orders$garch.s[i]
+  
+  for (j in 1:nrow(arma_orders)) {
+    ar <- arma_orders$ar[j]
+    ma <- arma_orders$ma[j]
+    
+    # Fit the GARCH model with ARMA orders
+    try({
+      model <- do_garch(garch.model = "sGARCH", 
+                        garch.r = garch.r, 
+                        garch.s = garch.s, 
+                        data = log_returns$daily.returns, 
+                        dist.model = "norm", 
+                        ar = ar, 
+                        ma = ma)
+      
+      # Extract the AIC and BIC
+      aic <- infocriteria(model$fit)[1]
+      bic <- infocriteria(model$fit)[2]
+      shib <- infocriteria(model$fit)[3]
+      han <- infocriteria(model$fit)[4]
+      
+      # Store the results
+      results <- rbind(results, data.frame(garch.r = garch.r, garch.s = garch.s,
+                                           ar = ar, ma = ma,
+                                           AIC = aic, BIC = bic,
+                                           SHIB = shib, HAN = han))
+    }, silent = TRUE)
+  }
+}
+
+# Find the model with the lowest AIC and BIC
+best_aic_model <- results[which.min(results$AIC), ]
+best_bic_model <- results[which.min(results$BIC), ]
+best_shib_model <- results[which.min(results$SHIB), ]
+best_han_model <- results[which.min(results$HAN), ]
+
+# Print the best models
+print("Best ARMA model by AIC:")
+print(best_aic_model)
+print("Best ARMA model by BIC:")
+print(best_bic_model)
+print("Best ARMA model by SHIB:")
+print(best_shib_model)
+print("Best ARMA model by HAN:")
+print(best_han_model)
+
+########## FINAL MODEL 
+garch <- do_garch(garch.model = "sGARCH", 
+                  garch.r = 1, 
+                  garch.s = 1, 
+                  data = log_returns$daily.returns, 
+                  dist.model = "norm", 
+                  ar = 1, 
+                  ma = 2)
+
+par(mfrow = c(1, 1))
+plot.ts(garch$residuals, main = "Residuals of GARCH model", ylab = "Residuals")
+plot.ts(garch$squared_residuals, main = "Squared Residuals of GARCH model", ylab = "Squared Residuals")
+
+# ACF analysis for residuals
+# This will allow you to check the adequacy of the GARCH(1,1) model by examining whether the residuals
+# and their squared values exhibit any significant autocorrelation. If the ACF and PACF plots show no
+# significant autocorrelation, and have a N(0,1) white noise behaviour, the model is considered a good fit for the data.
+par(mfrow = c(2, 2))
+acf(garch$residuals, main = "ACF of Standardized Residuals")
+pacf(garch$residuals, main = "PACF of Standardized Residuals")
+acf(garch$squared_residuals, main = "ACF of Squared Standardized Residuals")
+pacf(garch$squared_residuals, main = "PACF of Squared Standardized Residuals")
+par(mfrow = c(1, 1))
+
+########## FORECASTING #################################
+
